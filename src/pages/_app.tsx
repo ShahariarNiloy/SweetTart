5bf47013afa84d355af5a2668832000c5fd05cc3
ShahariarNiloy <shahariarniloyblv@gmail.com>
2023-06-19 13:53:22 +0600

    login and register modal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import Layout from "@/components/Layout";
+import LoginModal from "@/components/modals/LoginModal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <LoginModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
