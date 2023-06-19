0ffcfc96f27915a64f945e86674284b3cc9befa6
ShahariarNiloy <shahariarniloyblv@gmail.com>
2023-06-19 22:48:12 +0600

    edit modal added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Layout from "@/components/Layout";
+import EditModal from "@/components/modals/EditModal";
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
 import "@/styles/globals.css";
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps?.session}>
       <Toaster />
+      <EditModal />
       <RegisterModal />
       <LoginModal />
       <Layout>
