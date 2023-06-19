310008851e6104bd3e2a7c0b2155712560ca4b52
ShahariarNiloy <shahariarniloyblv@gmail.com>
2023-06-19 15:41:51 +0600

    toaster added

--- src/components/layout/SidebarTweetButton.tsx
+++ src/components/layout/SidebarTweetButton.tsx
@@ -2,22 +2,22 @@ import { useCallback } from "react";
 import { FaFeather } from "react-icons/fa";
 import { useRouter } from "next/router";
 
-// import useLoginModal from "@/hooks/useLoginModal";
+import useLoginModal from "@/hooks/useLoginModal";
 // import useCurrentUser from "@/hooks/useCurrentUser";
 
 const SidebarTweetButton = () => {
   const router = useRouter();
-  //   const loginModal = useLoginModal();
+  const loginModal = useLoginModal();
   //   const { data: currentUser } = useCurrentUser();
 
   const onClick = useCallback(() => {
     // if (!currentUser) {
     //   return loginModal.onOpen();
     // }
-
+    return loginModal.onOpen();
     router.push("/");
     //   }, [loginModal, router, currentUser]);
-  }, [, router]);
+  }, [loginModal, router]);
 
   return (
     <div onClick={onClick}>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,20 @@ import Layout from "@/components/Layout";
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
+      <Toaster />
       <RegisterModal />
       <LoginModal />
+      {/* <EditModal /> */}
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SessionProvider>
   );
 }
